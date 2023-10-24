cded4072316ea5f6c565df19224ae9eef4ae352c
Stijn121 <s.engelmoer@ziggo.nl>
2023-10-24 13:49:29 +0200

    box mouse events #3

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const app = Vue.createApp({
     data() {
         return {
+            url: 'https://www.youtube.com',
             showBooks: true,
-            title: 'empire',
-            author: 'stephan',
-            age: 22
+            books: [
+                { title: 'dagboek van een loser', author: 'jeff', img: 'assets/3.jpg', isFav: true},
+                { title: 'dagboek van een loser deel 2', author: 'jeff', img: 'assets/2.jpg', isFav: false},
+                { title: 'empire', author: 'steven', img: 'assets/1.jpg', isFav: false}
+            ],
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type, data)
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
 })
