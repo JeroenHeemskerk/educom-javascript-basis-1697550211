411e3d9e5b33ed0a6ceb94b2314de46b5fa376dc
Stijn121 <s.engelmoer@ziggo.nl>
2023-10-25 12:38:04 +0200

    video #4

---
+++ modal-project/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <Modal />
+    <input type="text" ref="name">
+    <button @click="handleClick">klik</button>
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: { Modal },
+  data() {
+    return {
+      title: 'Eerste vue app met titel'
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
