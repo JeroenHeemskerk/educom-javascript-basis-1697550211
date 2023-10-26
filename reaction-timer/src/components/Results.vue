00add46d3bf38429ecbe85c71a4ca211d942e918
Stijn121 <s.engelmoer@ziggo.nl>
2023-10-26 10:43:06 +0200

    video #6

---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <p>tijd: {{ score }} ms </p>
+        <p class="rank">{{ rank }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 200) {
+            this.rank = 'valsspeller'
+        } else if (this.score < 400) {
+            this.rank = 'snelle vingers'
+        } else {
+            this.rank = 'niet snel genoeg'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
