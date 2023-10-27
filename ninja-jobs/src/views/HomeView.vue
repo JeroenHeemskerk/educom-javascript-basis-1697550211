b20a647bc139d65fffba9e7ae8a4f88ab82b6c49
Stijn121 <s.engelmoer@ziggo.nl>
2023-10-27 15:36:53 +0200

    video # 9

---
+++ ninja-jobs/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed ipsam suscipit non, fuga, error repellat veniam necessitatibus magni earum, modi expedita itaque quas consectetur ducimus hic repellendus? Quisquam, asperiores! A.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero repellendus accusamus soluta pariatur debitis deserunt voluptatum eum perspiciatis delectus. Eaque deleniti voluptate obcaecati ex omnis fugit quod alias hic non.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
