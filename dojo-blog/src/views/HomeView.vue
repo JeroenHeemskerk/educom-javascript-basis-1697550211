0fe685b20e941fe741ae33c7d3003919088eaca3
Stijn121 <s.engelmoer@ziggo.nl>
2023-10-30 13:13:26 +0100

    eerste deel video #10

---
+++ dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <p>home...</p>
+    <p ref="p">name: {{ name }} en age: {{ age }}</p>
+    <button @click="handleClick">klik me</button>
+    <button @click="age++" >get 1 year older</button>
+    <input type="text" v-model="name">
+  </div>
+</template>
+
+<script>
+import { ref, reactive } from 'vue'
+
+export default {
+  name: 'HomeView',
+  setup() {
+
+    const name = ref('Mario')
+    const age = ref(35)
+
+    const handleClick = () => {
+      name.value = 'Luigi'
+      age.value = 30
+    }
+
+    return { 
+      name,
+      age,
+      handleClick
+    }
+  }
+}
+</script>
